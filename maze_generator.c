4e8ea0ba5b133c4f19782c9feec24e9b91d2ce95
Daniel Pelanek <72805357+Siigull@users.noreply.github.com>
2023-11-22 23:19:41 +0100

    Add files via upload

---
+++ maze_generator.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+typedef struct {
+  int rows;
+  int cols;
+  unsigned char *cells; // 0.bit -> left, 1.bit -> right, 2.bit -> top/bottom
+} Map;
+
+typedef struct {
+  int row;
+  int col;
+} Cell;
+
+Map* init_map(int rows, int cols) {
+  Map* map = malloc(sizeof(Map));
+  if(map == NULL) return NULL;
+    
+  map->cells = malloc(sizeof(unsigned char) * rows * cols);
+  if(map->cells == NULL) {
+    free(map);
+    return NULL;
+  }
+
+  map->rows = rows;
+  map->cols = cols;
+
+  for(int i=0; i < map->rows; i++) {
+    for(int j=0; j < map->cols; j++) {
+      map->cells[i*map->rows + j] = 0;
+    }
+  }
+
+  return map;
+}
+
+void free_map(Map* map) {
+  free(map->cells);
+  free(map);
+}
+
+void print_map(Map *map) {
+  printf("%d %d\n", map->rows, map->cols);
+  
+  for(int i=0; i < map->rows; i++) {
+    for(int j=0; j < map->cols; j++) {
+      printf("%d ", map->cells[i*map->rows + j]);
+    }
+    printf("\n");
+  }
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void shuffle(int arr[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+
+        swap(&arr[i], &arr[j]);
+    }
+}
+
+void generate_full(Map *map) {
+  for(int i=0; i < map->rows; i++) {
+    for(int j=0; j < map->cols; j++) {
+      map->cells[i*map->rows + j] = 7;
+    }
+  }
+}
+
+bool isborder(Map* map, int row, int col, int border) {
+  if((map->cells[row*map->cols + col]) & border) return true;
+  return false;
+}
+
+Cell get_move_cell(int move, bool rotated) {
+  Cell cell = {0, 0};
+  
+  if(move == 1) 
+    cell.col = -1;
+
+  else if(move == 2)  
+    cell.col = 1;
+
+  else { 
+    if(rotated) 
+      cell.row = -1;
+    
+    else 
+      cell.row = 1;
+  }
+
+  return cell;
+}
+
+bool is_outside(Map* map, int row, int col) {
+  return (row >= map->rows || row < 0 || col >= map->cols || col < 0);
+}
+
+bool generate_perfect_from_full(Map *map, int row, int col, bool **vis) {
+  if (is_outside(map, row, col) || vis[row][col]) return false;
+
+  vis[row][col] = true;
+   
+  bool rotated = ((row + col) % 2) == 0;
+
+  int moves[] = {1, 2, 4};
+  shuffle(moves, 3);
+
+  for(int i=0; i < 3; i++) {
+
+    Cell cell = get_move_cell(moves[i], rotated);
+
+    bool x = generate_perfect_from_full(map, row+cell.row, col+cell.col, vis);
+           
+    if(x) {
+      map->cells[row*map->rows + col] &= ~moves[i];
+      map->cells[(row + cell.row)*map->rows + col + cell.col] &= ~(moves[i] == 4? 4 : moves[i] ^ 0b11);
+    }
+      
+  }
+
+  return true;
+}
+
+
+int main(int argc, char** argv) {
+  srand(time(NULL));
+
+  if(argc != 3) {
+    printf("bad number of args\n");
+    return 0;
+  }
+  
+  Map* map = init_map(atoi(argv[1]), atoi(argv[2]));
+
+  bool** vis = calloc(map->rows, sizeof(bool*));
+  for(int i=0; i < map->rows; i++) {
+    vis[i] = calloc(map->cols, sizeof(bool));
+  }
+
+  generate_full(map);
+  generate_perfect_from_full(map, 0, 0, vis);
+
+  map->cells[0] &= ~1;
+  map->cells[(map->rows - 1) * map->cols + map->cols - 1] &= ~2;
+
+  print_map(map);
+  
+  return 0;
+}
---
+++ visualize_map.py
@@ -0,0 +1,101 @@
+import pygame
+import numpy as np
+from math import sqrt
+import sys
+
+def draw_triangle(j, i):
+    bot_left = ((j / 2) * side + pad, (i + 1) * height + pad)
+    bot_right = ((j / 2) * side + side + pad, (i + 1) * height + pad)
+    top = (((j/2) + 0.5) * side + pad, i * height + pad)
+
+    if (i+1, j+1) in path:
+        pygame.draw.polygon(gameDisplay, red, (bot_left, bot_right, top))
+    
+    if np.bitwise_and(maze[i][j], 4):
+        pygame.draw.aaline(gameDisplay, white, bot_left, bot_right, blend = 0)
+    if np.bitwise_and(maze[i][j], 1):
+        pygame.draw.aaline(gameDisplay, white, bot_left, top, blend = 0)
+    if np.bitwise_and(maze[i][j], 2):
+        pygame.draw.aaline(gameDisplay, white, top, bot_right, blend = 0)
+
+def draw_reversed_triangle(j, i):
+    top_left = ((j / 2) * side + pad, i * height + pad)
+    top_right = ((j / 2) * side + side + pad, i * height + pad)
+    bottom = (((j / 2) + 0.5) * side + pad, (i + 1) * height + pad)
+
+    if (i+1, j+1) in path:
+        pygame.draw.polygon(gameDisplay, red, (top_left, top_right, bottom))
+    
+    if np.bitwise_and(maze[i][j], 4):
+        pygame.draw.aaline(gameDisplay, white, top_left, top_right, blend = 0)
+    if np.bitwise_and(maze[i][j], 1):
+        pygame.draw.aaline(gameDisplay, white, top_left, bottom, blend = 0)
+    if np.bitwise_and(maze[i][j], 2):
+        pygame.draw.aaline(gameDisplay, white, bottom, top_right, blend = 0)
+
+with open(sys.argv[1]) as f:
+  rows,cols = np.fromfile(f, dtype=int, count=2, sep=" ")
+  maze = np.fromfile(f, dtype=int, count=cols*rows, sep=" ").reshape((rows,cols))
+
+print(maze)
+
+path = {}
+
+if(len(sys.argv) > 2):
+    i = 0
+    with open(sys.argv[2]) as f:
+        while 1:
+            line = f.readline()
+            if not line: 
+                break
+            line = line.split(',')
+            path[(int(line[0]), int(line[1]))] = i
+            i += 1
+
+print(path)
+
+pygame.init()
+
+white,black,red = (255,255,255),(0,0,0),(255,0,0)
+
+screen_size = 800
+
+gameDisplay = pygame.display.set_mode((screen_size, screen_size))
+
+side = screen_size / max(rows, cols) 
+height = sqrt((side**2) - (side/2)**2)
+font_size = 20
+pad = 5
+line_width = 3
+
+print(height)
+
+font_name = pygame.font.get_fonts()[0]
+
+font = pygame.font.SysFont(font_name, font_size)
+
+for i in range(0, rows):
+    # img = font.render(f'{i+1}', True, white)
+    # gameDisplay.blit(img, (pad / 3, i*height + pad + height / 4));
+
+    for j in range(0, cols):
+        # img = font.render(f'{j+1}', True, white)
+        # gameDisplay.blit(img, (j/2*side + pad + side / 2 - font_size / 4, pad/3));
+        
+        
+        if((j + i) % 2):
+            draw_triangle(j, i)
+        else:
+            draw_reversed_triangle(j, i)
+
+pygame.display.update()
+
+pygame.image.save(gameDisplay, "maze.png")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
